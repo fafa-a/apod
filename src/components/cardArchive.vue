124b744c0f01446621782351950c9c1c8e727574
fafa-a <fabienpuech1@gmail.com>
2021-06-07 14:40:57 +0200

    refacto(carArchive): ternary conditioninstead of v-if/v-else

--- src/components/cardArchive.vue
+++ src/components/cardArchive.vue
@@ -20,26 +20,19 @@
       border="solid 1px light-700 rounded-xl"
       m="b-4 x-auto"
     >
-      <span v-if="item.media_type == 'video'">
-        <img
-          :src="'https://i.ytimg.com/vi/' + videoURL + '/hqdefault.jpg'"
-          alt="item.title"
-          w="full"
-          h="300px"
-          object="cover center"
-          border="rounded-t-xl"
-        />
-      </span>
-      <span v-else>
-        <img
-          :src="item.url"
-          alt="item.title"
-          w="full"
-          h="300px"
-          object="cover center"
-          border="rounded-t-xl"
-        />
-      </span>
+      <img
+        :src="
+          video
+            ? 'https://i.ytimg.com/vi/' + videoURL + '/hqdefault.jpg'
+            : item.url
+        "
+        alt="item.title"
+        w="full"
+        h="300px"
+        object="cover center"
+        border="rounded-t-xl"
+        loading="lazy"
+      />
       <div font="body" flex="~ col" m="t-2" p="l-2">
         <p text="cool-gray-400">{{ item.date }}</p>
         <h2 text="lg cool-gray-800" m="t-1">{{ item.title }}</h2>
@@ -57,6 +50,7 @@ import rightArrow from "./rightArrow.vue"
 const slug = props.item.title.replaceAll(" ", "_")
 const itemURL = new URL(props.item.url)
 const videoURL = itemURL.pathname.slice(7)
+const video = props.item.media_type == "video"
 
 const props = defineProps({
   item: {
