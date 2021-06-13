d8976fc90699bac5e8211c08793548a348e60d9e
fafa-a <fabienpuech1@gmail.com>
2021-06-14 01:35:23 +0200

    fix(thepicture): fixed the :src:'url'

--- src/components/ThePicture.vue
+++ src/components/ThePicture.vue
@@ -4,7 +4,7 @@
       <iframe
         width="960"
         height="540"
-        src="url"
+        :src="url"
         title="YouTube video player"
         frameborder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
