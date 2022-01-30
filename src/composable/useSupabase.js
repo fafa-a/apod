c521078ca061dd5b248dbfcf05c301c472a695ae
fafa-a <fabienpuech1@gmail.com>
2022-01-30 23:46:17 +0100

    chore : deleted console.log

--- src/composable/useSupabase.js
+++ src/composable/useSupabase.js
@@ -6,7 +6,7 @@ const supabase = createClient(supabaseUrl, supabaseKey)
 
 const [date, month, year] = new Date().toLocaleDateString("fr-FR").split("/")
 const today = `${year}-${month}-${date}`
-console.log({ today })
+
 const picsOftheDay = async () => {
   let { data: apod, error } = await supabase
     .from("apod")
